82de5ecafb3529e72adfd2827bbba920512d3929
Balaji Ramachandrakumar <64825466+BalajiRamachandrakumar@users.noreply.github.com>
2022-12-20 16:11:59 +0530

    date command modification

--- automation5.sh
+++ automation5.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 ONE=14d
 
-Fourteendaysold=`date -v -${ONE}`
+#Fourteendaysold=`date -v -${ONE}` # this command  didnt worked due to -v option
+Fourteendaysold=`date --date='14 days ago' ` # this worked out for me 
 #Fourteendaysold=`date -d "$dataset_date - $ONE days" +%F`
 echo $Fourteendaysold
 
 TWO=7d
-sevendaysold=`date -v -${TWO}`
+#sevendaysold=`date -v -${TWO}` # this command  didnt worked due to -v option
+sevendaysold=`date --date='7days ago' ` # this worked out for me 
+
 #sevendaysold=`date -d "$dataset_date - $TWO days" +%F`
 echo $sevendaysold
 #date1=`echo $sevendaysold | sed 's/-//g’`
